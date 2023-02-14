1cdc48384dc290d972aa057587de3f71e5c49bac
lucasrenandns <lucasrenandns96@gmail.com>
2023-02-13 21:43:22 -0300

    Counter v2.0

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  function incrementCount() {
+    setCount(count + 1) 
+    document.getElementById("count").style.color = "#00d600"
+  }
+
+  function decrementCount() {
+    setCount(count - 1)
+    document.getElementById("count").style.color = "#df1d1b"
+  }
+
+  function resetCount() {
+    setCount(0)
+    document.getElementById("count").style.color = "#fff"
+  }
+
+  return (
+      <div className="container">
+        <h1>Contador</h1>
+
+        <span id='count'>{count}</span>
+
+        <div className="buttons">
+          <button type='button' onClick={incrementCount}>Increment</button>
+          <button type='button' onClick={resetCount}>Reset</button>
+          <button type='button' onClick={decrementCount}>Decrement</button>
+        </div>{/*End buttons */}
+      </div>//End container
+  ) 
+}
