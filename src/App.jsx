8f9e3c23a23daa5bc8a40ef1ef0d7ca4fa1e2ea2
lucasrenandns <lucasrenandns96@gmail.com>
2023-06-12 16:46:11 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
 
-  function incrementCount() {
-    setCount(count + 1) 
-    document.getElementById("count").style.color = "#00d700"
+  function incrementCounter() {
+    setCounter((counter) + 1) 
+    document.getElementById("counter").style.color = "#00d700"
   }
 
-  function decrementCount() {
-    setCount(count - 1)
-    document.getElementById("count").style.color = "#df1d1b"
+  function decrementCounter() {
+    setCounter((counter) - 1)
+    document.getElementById("counter").style.color = "#df1d1b"
   }
 
-  function resetCount() {
-    setCount(0)
-    document.getElementById("count").style.color = "#252525"
+  function resetCounter() {
+    setCounter(0)
+    document.getElementById("counter").style.color = "#252525"
   }
 
   return (
       <div className="container">
         <h1>Counter</h1>
 
-        <h2 id='count'>{count}</h2>
+        <h2 id='counter'>{counter}</h2>
 
         <div className="buttons">
-          <button type='button' onClick={incrementCount}>Increment</button>
-          <button type='button' onClick={resetCount}>Reset</button>
-          <button type='button' onClick={decrementCount}>Decrement</button>
+          <button type='button' onClick={incrementCounter}>Increment</button>
+          <button type='button' onClick={resetCounter}>Reset</button>
+          <button type='button' onClick={decrementCounter}>Decrement</button>
         </div>{/*End buttons*/}
       </div>//End container
   ) 
